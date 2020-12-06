2dba3333ae4fe9b127f8740f91d59722897335c7
hunarjain007 <55443281+hunarjain007@users.noreply.github.com>
2020-12-07 00:15:05 +0530

    feat: clickHandler

--- src/App.js
+++ src/App.js
@@ -5,24 +5,37 @@ const emojiDict = {
   "🏁": "Chequered Flag"
 };
 
+const emojiArray = Object.keys(emojiDict);
+
 export default function App() {
   const [emojiMeaning, setEmojiMeaning] = useState("");
 
   function onChangeHandler(emoji) {
     if (emoji in emojiDict) {
-      console.log(emoji);
+      // console.log(emoji);
       setEmojiMeaning(emojiDict[emoji]);
     } else {
-      console.log(emoji);
+      // console.log(emoji);
       setEmojiMeaning("Sorry the database doesn't have it.");
     }
+  }
+  function onClickHandler(emoji) {
+    emoji = emoji.target.textContent;
+    setEmojiMeaning(emojiDict[emoji]);
   }
   return (
     <div className="App">
       <h1>Emoji Interpreter</h1>
-      <input onChange={(event) => onChangeHandler(event.target.value)}>
-        {}
-      </input>
+      <input onChange={(event) => onChangeHandler(event.target.value)}></input>
+      <div>
+        {emojiArray.map((emoji) => {
+          return (
+            <span key={emoji} onClick={onClickHandler}>
+              {emoji}
+            </span>
+          );
+        })}
+      </div>
       <h1>{emojiMeaning}</h1>
     </div>
   );
