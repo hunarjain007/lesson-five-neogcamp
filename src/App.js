7fd788bdacdf936d04e1d1a04c708b2b10cd2f68
hunarjain007 <55443281+hunarjain007@users.noreply.github.com>
2020-12-06 23:58:15 +0530

    basic-boilerplate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
+const emojiDict = {
+  "🏁": "Chequered Flag"
+};
+
 export default function App() {
+  const [emojiMeaning, setEmojiMeaning] = useState("");
+
+  function onChangeHandler(emoji) {
+    if (emoji in emojiDict) {
+      console.log(emoji);
+      setEmojiMeaning(emojiDict[emoji]);
+    } else {
+      console.log(emoji);
+      setEmojiMeaning("Sorry the database doesn't have it.");
+    }
+  }
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Emoji Interpreter</h1>
+      <input onChange={(event) => onChangeHandler(event.target.value)}>
+        {}
+      </input>
+      <h1>{emojiMeaning}</h1>
     </div>
   );
 }
